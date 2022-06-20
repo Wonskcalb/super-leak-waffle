b06ef0bce037a35b12aec4ef841013b0e29e2645
Valentin Famelart <valentin.famelart@gitguardian.com>
2022-06-20 18:11:04 +0200

    weeeeeeeeeeeee

---
+++ newer_config.py
@@ -0,0 +1,11 @@
+# Place config variables here. One per line.
+
+# Enable Flask's debugging features. Should be False in production
+DEBUG = True
+
+DATABASE = {
+    "username": "dev",
+    "password": "db-vN3t7XEXnZQ38e6f",
+    "port": 5432,
+    "host": "https://db.nowhereoninter.net"
+}
