9f22f5f1f017048d606dda2e4fe821a3be9bd516
Valentin Famelart <valentin.famelart@gitguardian.com>
2022-06-20 18:00:49 +0200

    connect to db

--- config.py
+++ config.py
@@ -3,3 +3,9 @@
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
 
+DATABASE = {
+    "username": "dev",
+    "password": "db-vN3t7XEXnZQ38e6f",
+    "port": 5432,
+    "host": "https://db.nowhereoninter.net"
+}
